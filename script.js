21cb683fcd44956111a5e07edf153246c4f51e6b
Sagi6120 <102312477+Sagi6120@users.noreply.github.com>
2024-11-03 11:45:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const statusMessage = document.getElementById("statusMessage");
 
     turnOnButton.addEventListener("click", function() {
-        // Simulate turning the relay ON
-        statusMessage.textContent = "Relay TURNED ON"; // Update the status message
-        console.log("Relay turned ON"); // For debugging
+        // Simulate turning the relay ON - AKM
+        statusMessage.textContent = "Relay TURNED ON - AKM"; // Update the status message
+        console.log("Relay turned ON - AKM"); // For debugging
     });
 
     turnOffButton.addEventListener("click", function() {
-        // Simulate turning the relay OFF
-        statusMessage.textContent = "Relay TURNED OFF"; // Update the status message
-        console.log("Relay turned OFF"); // For debugging
+        // Simulate turning the relay OFF - NIO
+        statusMessage.textContent = "Relay TURNED OFF - NIO"; // Update the status message
+        console.log("Relay turned OFF - NIO"); // For debugging
     });
 });
