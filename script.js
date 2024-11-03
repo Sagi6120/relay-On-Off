8b4909906c5ca241067afe88cdc5bc4299f8ee02
Sagi6120 <102312477+Sagi6120@users.noreply.github.com>
2024-11-03 13:57:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ async function getRelayStatus() {
     try {
         const response = await fetch(`${RELAY_API_URL}/status`);
         if (!response.ok) throw new Error("Network response was not ok");
+        
         const result = await response.json();
         document.getElementById("relayStatus").textContent = result.relay_status;
     } catch (error) {
